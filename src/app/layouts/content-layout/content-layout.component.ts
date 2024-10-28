45802036ee41050cc8167148e7ba41a0be78fa28
ishansasika <ishansasika@gmail.com>
2024-10-28 23:19:13 +0530

    feat : Update layouts

--- src/app/layouts/content-layout/content-layout.component.ts
+++ src/app/layouts/content-layout/content-layout.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '@components/shell/footer/footer.component';
+import { HeaderComponent } from '@components/shell/header/header.component';
 
 @Component({
   selector: 'app-content-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './content-layout.component.html',
   styleUrl: './content-layout.component.css',
 })
