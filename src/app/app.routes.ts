94ec22dd11327aa5f12ecce28f61284ecd83a243
ishansasika <ishansasika@gmail.com>
2024-10-28 23:27:55 +0530

    feat: Add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import { BareboneLayoutComponent } from '@layouts/barebone-layout/barebone-layout.component';
+import { ContentLayoutComponent } from '@layouts/content-layout/content-layout.component';
+import { AboutComponent } from '@pages/about/about.component';
+import { HomeComponent } from '@pages/home/home.component';
+import { ErrorComponent } from '@pages/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ContentLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'about',
+        component: AboutComponent,
+      },
+    ],
+  },
+  {
+    path: '',
+    component: BareboneLayoutComponent,
+    children: [
+      {
+        path: 'error',
+        component: ErrorComponent,
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'error',
+  },
+];
--- src/app/pages/error/error.component.spec.ts
+++ src/app/pages/error/error.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { 404Component } from './404.component';
+import { ErrorComponent } from './error.component';
 
-describe('404Component', () => {
-  let component: 404Component;
-  let fixture: ComponentFixture<404Component>;
+describe('ErrorComponent', () => {
+  let component: ErrorComponent;
+  let fixture: ComponentFixture<ErrorComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [404Component]
+      imports: [ErrorComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(404Component);
+    fixture = TestBed.createComponent(ErrorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
