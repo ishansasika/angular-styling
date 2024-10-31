5ebb2c132b591d5473bba31648e54f28f462899b
ishansasika <ishansasika@gmail.com>
2024-10-31 21:34:40 +0530

    feat : Add home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,8 +5,12 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  heroImages: string[] = [
+    'assets/images/hero-1.webp',
+    'assets/images/hero-2.webp',
+    'assets/images/hero-3.webp',
+  ];
 }
