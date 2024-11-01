fed019e2f109bdde89625f5d0cc05d2d24bcd9e6
ishansasika <ishansasika@gmail.com>
2024-11-01 09:18:33 +0530

    feat : Add virtual scrolling

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '@shared/components/button/button.component';
 import { HorizontalScrollerDirective } from '@shared/directives/horizontal-scroller/horizontal-scroller.directive';
-
+import { ScrollingModule } from '@angular/cdk/scrolling';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HorizontalScrollerDirective, ButtonComponent],
+  imports: [HorizontalScrollerDirective, ButtonComponent, ScrollingModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
@@ -18,6 +18,7 @@ export class HomeComponent {
 
   typography: string[] = ['tiny', 'xs', 'sm', 'md', 'lg', 'xl', '2xl'];
 
+  scrollArray = Array.from({ length: 50 }, (_, i) => i + 1);
   array = Array.from({ length: 30 }, (_, i) => i + 1);
 
   activeIndex!: number;
